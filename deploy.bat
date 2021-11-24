a94cfcb38cd3ec42b5fcae980d25ff3c0e86754d
KO BOUNG JUN <koboungjun94@gmail.com>
2021-11-24 12:26:12 +0900

    1

--- deploy.bat
+++
@@ -1,2 +0,0 @@
-ssh -i "C:front.pem" ubuntu@54.180.124.220 "pkill -f node"
-ssh -i"C:front.pem" ubuntu@54.180.124.220 "cd /home/ubuntu/web/app/Partner/partner-service; git pull; npm install; npm run build; nohup npm start 1>/dev/null 2>&1 &"
